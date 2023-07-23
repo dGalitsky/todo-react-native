2a527aa9082284f2a4dfcf6d6915c80df649731b
Dmytro Halytskyi <galitskiy.dmitriy@gmail.com>
2023-07-23 17:41:13 +0100

    Add fake API

---
+++ src/api/api.test.ts
@@ -0,0 +1,53 @@
+import AsyncStorage from "@react-native-async-storage/async-storage"
+import { v4 as uuidv4 } from "uuid"
+import { STORAGE_KEY, api } from "."
+
+const mockGetItem = AsyncStorage.getItem as jest.Mock
+const mockSetItem = AsyncStorage.setItem as jest.Mock
+
+describe("Api", () => {
+  beforeEach(() => {
+    mockGetItem.mockReset()
+    mockSetItem.mockReset()
+  })
+
+  it("returns empty array if no todos", async () => {
+    mockGetItem.mockResolvedValue(null)
+    expect(await api.getTodos()).toEqual([])
+  })
+
+  it("adds a todo", async () => {
+    const title = "New Todo"
+    mockGetItem.mockResolvedValue(JSON.stringify([]))
+    const id = await api.addTodo(title)
+    expect(mockSetItem).toHaveBeenCalledWith(
+      STORAGE_KEY,
+      JSON.stringify([{ id, title }])
+    )
+  })
+
+  it("removes a todo by id", async () => {
+    const id = uuidv4()
+    mockGetItem.mockResolvedValue(
+      JSON.stringify([{ id, title: "Existing Todo" }])
+    )
+    await api.removeTodoById(id)
+    expect(AsyncStorage.setItem).toHaveBeenCalledWith(
+      STORAGE_KEY,
+      JSON.stringify([])
+    )
+  })
+
+  it("updates a todo by id", async () => {
+    const id = uuidv4()
+    const title = "Updated Todo"
+    mockGetItem.mockResolvedValue(
+      JSON.stringify([{ id, title: "Existing Todo" }])
+    )
+    await api.updateTodoById(id, title)
+    expect(mockSetItem).toHaveBeenCalledWith(
+      STORAGE_KEY,
+      JSON.stringify([{ id, title }])
+    )
+  })
+})
---
+++ src/api/index.ts
@@ -0,0 +1,54 @@
+import AsyncStorage from "@react-native-async-storage/async-storage"
+import { v4 as uuidv4 } from "uuid"
+
+export const STORAGE_KEY = "todos"
+
+/**
+ * Todo interface that uses AsyncStorage as a fake API
+ */
+class Api {
+  async getTodos() {
+    const todos = await AsyncStorage.getItem(STORAGE_KEY)
+    if (!todos) return []
+
+    try {
+      return JSON.parse(todos) as ITodo[]
+    } catch {
+      return []
+    }
+  }
+
+  async addTodo(title: string) {
+    const id = uuidv4()
+    const todo = {
+      id,
+      title,
+    }
+
+    const todos = await this.getTodos()
+    todos.unshift(todo)
+    await AsyncStorage.setItem(STORAGE_KEY, JSON.stringify(todos))
+
+    return id
+  }
+
+  async removeTodoById(id: string) {
+    const todos = await this.getTodos()
+    const index = todos.findIndex(todo => todo.id === id)
+    if (index === -1) return
+
+    todos.splice(index, 1)
+    await AsyncStorage.setItem(STORAGE_KEY, JSON.stringify(todos))
+  }
+
+  async updateTodoById(id: string, title: string) {
+    const todos = await this.getTodos()
+    const index = todos.findIndex(todo => todo.id === id)
+    if (index === -1) return
+
+    todos[index].title = title
+    await AsyncStorage.setItem(STORAGE_KEY, JSON.stringify(todos))
+  }
+}
+
+export const api = new Api()
